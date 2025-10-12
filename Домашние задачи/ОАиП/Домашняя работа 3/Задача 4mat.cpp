fea6cbe5984f228434ec5bcf8fbbd6015c5e6549
Любимова Алиса <lubimovaalisa57@gmail.com>
2025-10-12 18:18:17 +0300

    Добавила 4 задачу по матрицам

--- Домашние задачи/ОАиП/Домашняя работа 3/Задача 4mat.cpp
+++ Домашние задачи/ОАиП/Домашняя работа 3/Задача 4mat.cpp
@@ -1,47 +1,54 @@
 #include <iostream>
 
-int main() {
+int main() 
+{
     int N;
-    std::cout << "Введите размер матрицы N от 1 до 100: ";
+    std::cout << "Введите размер матрицы N( от 1 до 100): ";
     std::cin >> N;
     
-    int matrix[N][N] = {0};
+    int matrix[N][N];
+    int num = 1;       // Начинаем заполнение с числа 1
+    int current_layer = 0;  // Текущий слой спирали (начинается с внешнего слоя)
     
-    int num = 1;
-    int top = 0, bottom = N - 1;
-    int left = 0, right = N - 1;
-    
-    while (num <= N * N) {
-        // Заполняем верхнюю строку слева направо
-        for (int j = left; j <= right; j++) {
-            matrix[top][j] = num;
+    // Основной цикл для заполнения слоев спирали
+    // Количество слоев = N/2 (для N=3 будет 1 слой, для N=4 - 2 слоя и т.д.)
+    for (int k = 0; k < N / 2; k++) {
+        // ПЕРВЫЙ ЦИКЛ: Заполнение ВЕРХНЕЙ СТРОКИ текущего слоя СЛЕВА НАПРАВО
+        // matrix[current_layer][j] - строка current_layer фиксирована, j меняется от current_layer до N-current_layer-1
+        for (int j = current_layer; j < N - current_layer; j++) {
+            matrix[current_layer][j] = num;
             num++;
         }
-        top++;
         
-        // Заполняем правый столбец сверху вниз
-        for (int i = top; i <= bottom; i++) {
-            matrix[i][right] = num;
+        // ВТОРОЙ ЦИКЛ: Заполнение ПРАВОГО СТОЛБЦА текущего слоя СВЕРХУ ВНИЗ
+        // matrix[i][N-1-current_layer] - столбец N-1-current_layer фиксирован, i меняется от current_layer+1 до N-current_layer-1
+        for (int i = current_layer + 1; i < N - current_layer; i++) {
+            matrix[i][N - 1 - current_layer] = num;
             num++;
         }
-        right--;
         
-        // Заполняем нижнюю строку справа налево
-        for (int j = right; j >= left; j--) {
-            matrix[bottom][j] = num;
+        // ТРЕТИЙ ЦИКЛ: Заполнение НИЖНЕЙ СТРОКИ текущего слоя СПРАВА НАЛЕВО
+        // matrix[N-1-current_layer][N-j-1] - строка N-1-current_layer фиксирована, j меняется от current_layer+1 до N-current_layer-1 (в обратном порядке)
+        for (int j = current_layer + 1; j < N - current_layer; j++) {
+            matrix[N - 1 - current_layer][N - j - 1] = num;
             num++;
         }
-        bottom--;
         
-        // Заполняем левый столбец снизу вверх
-        for (int i = bottom; i >= top; i--) {
-            matrix[i][left] = num;
+        // ЧЕТВЕРТЫЙ ЦИКЛ: Заполнение ЛЕВОГО СТОЛБЦА текущего слоя СНИЗУ ВВЕРХ
+        // matrix[N-i-1][current_layer] - столбец current_layer фиксирован, i меняется от current_layer+1 до N-current_layer-2 (в обратном порядке)
+        for (int i = current_layer + 1; i < N - current_layer - 1; i++) {
+            matrix[N - i - 1][current_layer] = num;
             num++;
         }
-        left++;
+        
+        current_layer++;
     }
     
-    // Вывод матрицы
+    // Обработка центрального элемента для матриц с НЕЧЕТНЫМ размером
+    if (N % 2 != 0) {
+        matrix[N / 2][N / 2] = num;
+    }
+
     std::cout << "Спиральная матрица:" << std::endl;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
