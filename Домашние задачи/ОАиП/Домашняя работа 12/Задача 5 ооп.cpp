83fc051243bb8df62d70b387dd97a3eecdbfcdc1
Любимова Алиса <lubimovaalisa57@gmail.com>
2025-12-09 00:09:20 +0300

    Добавила 5 задачу

---
+++ Домашние задачи/ОАиП/Домашняя работа 12/Задача 5 ооп.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Publication {
+protected:
+    string title;
+};
+
+class Book : public Publication {
+private:
+    string author;
+    int year;
+    
+public:
+    Book(string t, string a, int y) {
+        title = t;
+        author = a;
+        year = y;
+    }
+    
+    void displayInfo() {
+        cout << "Название: " << title << ", Автор: " << author << ", Год выпуска: " << year << endl;
+    }
+};
+
+int main() {
+    Book book("Война и мир", "Лев Толстой", 1869);
+    book.displayInfo();
+    
+    return 0;
+}
